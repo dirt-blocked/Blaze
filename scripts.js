9730ea0f0071f60ad2d7d31357929d96600dda88
dirt-blocked <149276820+dirt-blocked@users.noreply.github.com>
2024-09-07 11:39:05 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 const three = document.getElementById("three");
 
 three.addEventListener('click', function() {
-    window.location.href = 'Pages/three.html';
+    window.location.href = 'pages/three.html';
 });
